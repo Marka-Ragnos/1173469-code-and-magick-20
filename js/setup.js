25f7477dbf174edfcc3cd354ac9c4b964b80cda0
Саня <dunkan-1@yandex.ru>
2020-06-20 23:02:22 +0300

    Учебный проект: в движении v.2

--- js/setup.js
+++ js/setup.js
@@ -1,45 +1,47 @@
 'use strict';
+(function () {
 
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setup.querySelector('.setup-close');
-var setupUserName = setup.querySelector('.setup-user-name');
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var setupUserName = setup.querySelector('.setup-user-name');
 
-function onPopupEscPress(evt) {
-  if (evt.key === 'Escape' && setupUserName !== document.activeElement) {
-    evt.preventDefault();
-    closePopup();
+  function onPopupEscPress(evt) {
+    if (evt.key === 'Escape' && setupUserName !== document.activeElement) {
+      evt.preventDefault();
+      closePopup();
+    }
   }
-}
 
-function openPopup() {
-  setup.classList.remove('hidden');
+  function openPopup() {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  }
 
-  document.addEventListener('keydown', onPopupEscPress);
-}
+  function closePopup() {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  }
 
-function closePopup() {
-  setup.classList.add('hidden');
-
-  document.removeEventListener('keydown', onPopupEscPress);
-}
-
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
-
-setupOpen.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Enter') {
+  setupOpen.addEventListener('click', function () {
     openPopup();
-  }
-});
+  });
 
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      openPopup();
+    }
+  });
 
-setupClose.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Enter') {
+  setupClose.addEventListener('click', function () {
     closePopup();
-  }
-});
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      closePopup();
+    }
+  });
+
+})();
+
