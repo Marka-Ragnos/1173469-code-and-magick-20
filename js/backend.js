2adea9149690d2f1af8ce01f5e0afd6ac33971bc
Саня <dunkan-1@yandex.ru>
2020-06-24 17:16:49 +0300

    Учебный проект: pimp my mage v.1

---
+++ js/backend.js
@@ -0,0 +1,70 @@
+'use strict';
+
+(function () {
+  var Url = {
+    DOWNLOAD: 'https://javascript.pages.academy/code-and-magick/data',
+    UPLOAD: 'https://javascript.pages.academy/code-and-magick'
+  };
+
+  var ServerCode = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    ENTERNAL_ERROR: 500
+  };
+
+
+  function createXHR(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case ServerCode.SUCCESS:
+          onLoad(xhr.response);
+          break;
+        case ServerCode.BAD_REQUEST:
+          onError('Произошла ошибка сервера: неверный запрос');
+          break;
+        case ServerCode.NOT_FOUND:
+          onError('Произошла ошибка сервера: запрашиваемый ресурс не найден');
+          break;
+        case ServerCode.ENTERNAL_ERROR:
+          onError('Произошла внутренняя ошибка сервера');
+          break;
+        default:
+          onError('Произошла ошибка сервера: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос к серверу не успел выполниться за отведённое время');
+    });
+
+    return xhr;
+  }
+
+  function loadData(onLoad, onError) {
+    var xhr = createXHR(onLoad, onError);
+
+    xhr.open('GET', Url.DOWNLOAD);
+    xhr.send();
+  }
+
+  function saveData(data, onLoad, onError) {
+    var xhr = createXHR(onLoad, onError);
+
+    xhr.open('POST', Url.UPLOAD);
+    xhr.send(data);
+  }
+
+  window.backend = {
+    load: loadData,
+    save: saveData
+  };
+})();
--- js/util.js
+++ js/util.js
@@ -2,16 +2,17 @@
 
 (function () {
 
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  // var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var fragment = document.createDocumentFragment();
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var wizards = getRandomWizards(4);
-  getInsertWizards(4);
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
+  // var wizards = getRandomWizards(4);
+  // getInsertWizards(4);
 
   window.util = {
     WIZARD_COAT_COLOR: WIZARD_COAT_COLOR,
@@ -24,21 +25,8 @@
     return Math.floor(Math.random() * Math.floor(lengthArray));
   }
 
-  function getRandomWizards(quantityWizards) {
-    var wizardArray = [];
-
-    for (var i = 0; i < quantityWizards; i++) {
-      wizardArray[i] = {
-        name: WIZARD_NAMES[getRandomArray(WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomArray(WIZARD_SURNAMES.length)],
-        coatColor: WIZARD_COAT_COLOR[getRandomArray(WIZARD_COAT_COLOR.length)],
-        eyesColor: WIZARD_EYES_COLOR[getRandomArray(WIZARD_EYES_COLOR.length)]
-      };
-    }
-    return wizardArray;
-  }
-
-  function getInsertWizards(quantityWizards) {
-    for (var i = 0; i < quantityWizards; i++) {
+  function getInsertWizards(wizards) {
+    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
       fragment.appendChild(renderWizard(wizards[i]));
     }
     return similarListElement.appendChild(fragment);
@@ -53,4 +41,60 @@
 
     return wizardElement;
   }
+
+  // ===============  Учебный проект: pimp my mage  ====================== //
+
+  var setup = document.querySelector('.setup');
+  var setupForm = setup.querySelector('.setup-wizard-form');
+  var setupFormBtn = setupForm.querySelector('.setup-submit');
+
+
+  var sendForm = function () {
+    setup.classList.add('hidden');
+    setupFormBtn.disabled = false;
+  };
+
+  function showError(errorMessage) {
+    createElementError(errorMessage);
+    setupFormBtn.disabled = false;
+  }
+
+  function onFormSubmit(evt) {
+    evt.preventDefault();
+    setupFormBtn.disabled = true;
+    window.backend.save(new FormData(setupForm), sendForm, showError);
+  }
+
+  function createElementError(errorMessage) {
+    var errorContainer = document.createElement('div');
+
+    function hideError() {
+      errorContainer.classList.add('hidden');
+    }
+    errorContainer.textContent = errorMessage;
+    errorContainer.classList.add('error');
+    document.body.appendChild(errorContainer);
+    setTimeout(hideError, 3000);
+  }
+
+  function createSimilarWizards(wizards) {
+    getInsertWizards(wizards);
+    setup.querySelector('.setup-similar').classList.remove('hidden');
+  }
+
+  setupForm.addEventListener('submit', onFormSubmit);
+  window.backend.load(createSimilarWizards, showError);
+
+  // function getRandomWizards(quantityWizards) {
+  //   var wizardArray = [];
+
+  //   for (var i = 0; i < quantityWizards; i++) {
+  //     wizardArray[i] = {
+  //       name: WIZARD_NAMES[getRandomArray(WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomArray(WIZARD_SURNAMES.length)],
+  //       coatColor: WIZARD_COAT_COLOR[getRandomArray(WIZARD_COAT_COLOR.length)],
+  //       eyesColor: WIZARD_EYES_COLOR[getRandomArray(WIZARD_EYES_COLOR.length)]
+  //     };
+  //   }
+  //   return wizardArray;
+  // }
 })();
